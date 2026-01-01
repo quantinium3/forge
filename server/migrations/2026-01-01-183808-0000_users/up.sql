75912ea97d277077ce2dc55dbec7ef60a25e5839
quantinium3 <quant@quantinium.dev>
2026-01-02 00:20:22 +0530

    created users table

---
+++ server/migrations/2026-01-01-183808-0000_users/up.sql
@@ -0,0 +1,22 @@
+-- Your SQL goes here
+create table users (
+	id uuid primary key default gen_random_uuid(),
+	username varchar(255) unique not null,
+	email varchar(255) unique not null,
+	email_verified boolean not null default false,
+
+	role varchar(255) not null default 'user',
+
+	is_active boolean not null default true,
+	is_banned boolean not null default false,
+	banned_until timestampz,
+	ban_reason varchar(255),
+
+	created_at timestamptz not null default now(),
+	updated_at timestamptz not null default now(),
+	last_login timestamptz,
+
+	index idx_email (email),
+	index idx_username (username),
+	index idx_role (role)
+);
