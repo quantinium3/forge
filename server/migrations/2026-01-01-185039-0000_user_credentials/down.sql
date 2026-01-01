996995811937d6e3749a8160fd4ca188a7898b20
quantinium3 <quant@quantinium.dev>
2026-01-02 00:44:04 +0530

    created user_credentials table

---
+++ server/migrations/2026-01-01-185039-0000_user_credentials/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+drop table if exists user_credentials;
+drop index if exists idx_user_id;
---
+++ server/migrations/2026-01-01-185039-0000_user_credentials/up.sql
@@ -0,0 +1,21 @@
+-- Your SQL goes here
+create table user_credentials (
+	id uuid primary key default gen_random_uuid(),
+	user_id uuid unique not null references users(id) on delete cascade,
+	password varchar(255) not null,
+	password_changed_at timestamptz not null default now(),
+
+	is_locked boolean not null default false,
+	locked_until timestamptz,
+	failed_login_attempts int not null default 0,
+	last_failed_login_at timestamptz,
+
+	totp_secret varchar(6),
+	totp_enabled boolean not null default false,
+	backup_codes text[],
+
+	created_at timestamptz not null default now(),
+	updated_at timestamptz not null default now(),
+);
+
+create index idx_user_id on user_credentials (user_id);
