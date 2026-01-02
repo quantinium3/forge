4baf5d573f2d4fa80edd7147495150accbb0ec39
quantinium3 <quant@quantinium.dev>
2026-01-03 02:02:42 +0530

    increase max length for username

--- forge-client/src/routes/sign-up/schema.ts
+++ forge-client/src/routes/sign-up/schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const signUpSchema = z.object({
 	username: z.string({ error: "Username is required" })
 		.min(3, { error: "Username must be at least 3 characters long" })
-		.max(20, { error: "Username must be at most 20 characters long" }),
+		.max(32, { error: "Username must be at most 20 characters long" }),
 	email: z.email({ error: "Invalid email" }),
 	password: z.string({ error: "Password is required" })
 		.min(8, { error: "Password must be at least 8 characters long" })
